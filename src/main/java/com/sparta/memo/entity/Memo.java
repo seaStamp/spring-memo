c295ea813d14875b40e089f27f9423fac906b614
seastamp <seastamp10@gmail.com>
2023-11-06 16:41:04 +0900

    modify : Spring jpa 설정

--- src/main/java/com/sparta/memo/entity/Memo.java
+++ src/main/java/com/sparta/memo/entity/Memo.java
@@ -1,16 +1,23 @@
 package com.sparta.memo.entity;
 
 import com.sparta.memo.dto.MemoRequestDto;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
+@Table(name = "memo")
 @NoArgsConstructor
 public class Memo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name="username", nullable = false)
     private String username;
+    @Column(name="contents", nullable = false, length = 500)
     private String contents;
 
     public Memo(MemoRequestDto requestDto) {
