b5a061ab0ceec30c2a24f8a270217a756d8d5f22
seastamp <seastamp10@gmail.com>
2023-11-06 17:54:28 +0900

    feat: Query Methods 적용

--- src/main/java/com/sparta/memo/entity/Memo.java
+++ src/main/java/com/sparta/memo/entity/Memo.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @Setter
 @Table(name = "memo")
 @NoArgsConstructor
-public class Memo {
+public class Memo extends Timestamped {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
